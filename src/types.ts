ef94d6d55d15f51da168f95eb470135964b463f1
Denis Voloshin <fl.devol@gmail.com>
2023-08-12 15:43:32 +0300

    changes

--- src/types.ts
+++ src/types.ts
@@ -3,4 +3,13 @@ export type Money = {
   currency: Currency | null;
 };
 
-export type Currency = "USD" | "GBP" | "EUR" | "BRL" | "RUB" | "ILS" | "RON";
+export type Currency = "AUD" | "BDT" | "BGN" | "BRL" | "CAD" | "CHF" | "CLP" | "CNY"
+  | "CZK" | "DKK" | "EUR" | "FJD" | "GBP" | "GEL" | "GHS" | "GMD" | "GTQ" | "GYD"
+  | "HKD" | "HRK" | "HTG" | "HUF" | "ILS" | "INR" | "ISK" | "JMD" | "JOD" | "JPY"
+  | "KES" | "KHR" | "KRW" | "KWD" | "LAK" | "LBP" | "LKR" | "MAD" | "MDL" | "MKD"
+  | "MMK" | "MOP" | "MUR" | "MVR" | "MWK" | "MXN" | "MYR" | "NAD" | "NGN" | "NOK" 
+  | "NPR" | "NZD" | "OMR" | "PAB" | "PEN" | "PGK" | "PHP" | "PKR" | "PLN" | "PYG"
+  | "QAR" | "RON" | "RSD" | "RUB" | "SAR" | "SCR" | "SEK" | "SGD" | "SLL" | "SOS"
+  | "SSP" | "STN" | "SYP" | "SZL" | "THB" | "TJS" | "TMT" | "TND" | "TOP" | "TRY"
+  | "TTD" | "TWD" | "TZS" | "UAH" | "UGX" | "USD" | "UYU" | "UZS" | "VEF" | "VND"
+  | "VUV" | "WST" | "XAF" | "XCD" | "XOF" | "YER" | "ZAR" | "ZMW" | "ZWL";
