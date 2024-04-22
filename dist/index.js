4db7a9df154db37f6e828b35657dc0a6f0fc2406
Denis Voloshin <fl.devol@gmail.com>
2024-04-23 00:16:24 +0300

    changes

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.symbols = void 0;
 exports.symbols = {
+    AED: ["د.إ", "DH", "AED"],
+    ARS: ["ARS"],
     AUD: ["A$", "AU$", "AUD"],
     BDT: ["৳", "TAKA", "BDT"],
     BGN: ["ЛВ", "BGN"],
@@ -10,8 +12,10 @@ exports.symbols = {
     CHF: ["FR", "CHF"],
     CLP: ["CLP"],
     CNY: ["¥", "圓", "CNY"],
+    COP: ["COP"],
     CZK: ["KČ", "CZK"],
     DKK: ["DKK"],
+    EGP: ["EGP"],
     EUR: ["€", "EURO", "EUR"],
     FJD: ["FJ$", "FJD"],
     GBP: ["£", "GBP"],
@@ -24,7 +28,8 @@ exports.symbols = {
     HRK: ["KN", "HRK"],
     HTG: ["G", "HTG"],
     HUF: ["FT", "HUF"],
-    ILS: ["₪", "ILS"],
+    IDR: ["IDR"],
+    ILS: ["₪", "NIS", "ILS"],
     INR: ["₹", "RS", "RP", "INR"],
     ISK: ["ISK"],
     JMD: ["J$", "JMD"],
@@ -37,7 +42,7 @@ exports.symbols = {
     LAK: ["₭", "LAK"],
     LBP: ["ل.ل", "L£", "LBP"],
     LKR: ["LKR"],
-    MAD: ["د.م.", "DH", "MAD"],
+    MAD: ["د.م.", "MAD"],
     MDL: ["L", "MDL"],
     MKD: ["ДЕН", "MKD"],
     MMK: ["K", "MMK"],
